ee93c7b356ea553a593cfde3c30c8107587b4130
HP <HP@BOTNIMOUS>
2022-11-01 23:50:04 +0530

    Add project files.

---
+++ factorial/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, number, fact;
+            Console.WriteLine("Enter the Number");
+            number = int.Parse(Console.ReadLine());
+            fact = 1;
+            for (i = 1; i <=number; i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("\nFactorial of Given Number is: " + fact);
+            Console.ReadLine();
+        }
+    }
+}
